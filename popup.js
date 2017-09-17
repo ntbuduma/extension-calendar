419edf0d05140b8ba8443afd37f2667be8f483dd
Abhijit Mudigonda <abhijit.mudigonda@gmail.com>
2017-09-17 04:23:40 -0400

    finished debug

--- popup.js
+++ popup.js
@@ -10,7 +10,7 @@ $(document).keypress(function(e) {
 });
 
 function nextXEvents() {
-  $.post("http://localhost:5000/view_event_list", {}, function(raw_data) {
+  $.get("http://localhost:5000/get_event_list", {}, function(raw_data) {
     //raw_data = '{"0": {"date": "09-16", "location": "", "name": "HackMIT 2017", "time": "11:00:00-04:00"}, "1": {"date": "09-23", "location": "", "name": "Palantir", "time": "12:00:00-04:00"}, "2": {"date": "09-23", "location": "", "name": "Hanna\'s birthday", "time": "22:00:00-04:00"}, "3": {"date": "10-05", "location": "", "name": "14.02 Exam 1", "time": "07:30:00-04:00"}, "4": {"date": "10-05", "location": "", "name": "Tony\'s Birthday!", "time": "18:00:00-04:00"}, "5": {"date": "10-12", "location": "", "name": "Arangetram Trip", "time": "21:30:00-04:00"}, "6": {"date": "11-02", "location": "", "name": "14.02 Exam 2", "time": "19:30:00-04:00"}, "7": {"date": "11-18", "location": "", "name": "Splash", "time": ""}, "8": {"date": "12-02", "location": "", "name": "Putnam", "time": ""}, "9": {"date": "12-07", "location": "", "name": "14.02 Exam 3", "time": "19:30:00-05:00"}}';
     data = JSON.parse(raw_data);
     var $e = $('#events')
@@ -26,7 +26,7 @@ function nextXEvents() {
 
 
 function nextWeekEvents() {
-  $.post("http://localhost:5000/view_event_grid", {}, function(raw_data) {
+  $.get("http://localhost:5000/get_event_grid", {}, function(raw_data) {
     // data = []
     // for (c = 0; c < 168; c++) {
     //   if (c % 13 === 0) {
@@ -52,7 +52,7 @@ function nextWeekEvents() {
       $e.append("<tr>")
       for (j = 0; j < 7; j++) {
         idx = i*7 + j 
-        $e.append("<td class='" + data[idx]["status"] + "'></td>")
+        $e.append("<td class='" + data[idx] + "'></td>")
       }
       $e.append("</tr>")
     }
